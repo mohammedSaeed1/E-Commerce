dfd6e73ab1acce6950ae76f002a2ad65d81d7c7d
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-09 23:16:56 +0200

    fix

--- src/components/CartEmpty/CartEmpty.jsx
+++ src/components/CartEmpty/CartEmpty.jsx
@@ -4,7 +4,7 @@ export default function CartEmpty() {
   return (
     <>
       <section className="text-center flex justify-center items-center h-[70vh] pt-2">
-        <h3 className="md:text-3xl text-2xl  font-bold text-green-600">
+        <h3 className="md:text-3xl text-2xl  font-bold text-green-600 block">
           Cart is Empty !
         </h3>
         <Link to={`/products`}>
