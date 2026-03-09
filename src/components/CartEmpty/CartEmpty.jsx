f475195754baf930df483beeb3029ecedca5d9ae
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-09 22:56:06 +0200

    fix

--- src/components/CartEmpty/CartEmpty.jsx
+++ src/components/CartEmpty/CartEmpty.jsx
@@ -1,8 +1,8 @@
 export default function CartEmpty() {
   return (
     <>
-      <section className="text-center flex justify-center items-center h-[70vh] pt-16">
-        <h3 className="md:text-3xl text-2xl py-8 font-bold text-green-600">
+      <section className="text-center flex justify-center items-center h-[70vh] py-10">
+        <h3 className="md:text-3xl text-2xl  font-bold text-green-600">
           Cart is Empty !
         </h3>
       </section>
