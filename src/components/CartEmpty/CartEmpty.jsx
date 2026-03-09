f0b882f0e333ede01d3f92c061983547ebd8639f
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-03-09 23:19:52 +0200

    fix

--- src/components/CartEmpty/CartEmpty.jsx
+++ src/components/CartEmpty/CartEmpty.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function CartEmpty() {
   return (
     <>
-      <section className="text-center flex justify-center items-center h-[70vh] pt-2">
-        <h3 className="md:text-3xl text-2xl  font-bold text-green-600 block">
+      <section className="text-center h-[70vh] pt-2">
+        <h3 className="md:text-3xl text-2xl  font-bold text-green-600 ">
           Cart is Empty !
         </h3>
         <Link to={`/products`}>
